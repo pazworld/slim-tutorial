ec66dba598919d84fef0bbc22501b20c3d1b6683
pazworld <paz@moudamepo.net>
2020-01-20 19:13:00 +0900

    Add route templates

--- src/routes.php
+++ src/routes.php
@@ -7,6 +7,34 @@ use Slim\Http\Response;
 return function (App $app) {
     $container = $app->getContainer();
 
+    // 一覧表示
+    $app->get('/tickets', function (Request $request, Response $response) {
+    });
+
+    // 新規作成用フォームの表示
+    $app->get('/tickets/create', function (Request $request, Response $response) {
+    });
+
+    // 新規作成
+    $app->post('/tickets', function (Request $request, Response $response) {
+    });
+
+    // 表示
+    $app->get('/tickets/{id}', function (Request $request, Response $response) {
+    });
+
+    // 編集用フォームの表示
+    $app->get('/tickets/{id}/edit', function (Request $request, Response $response) {
+    });
+
+    // 更新
+    $app->put('/tickets/{id}', function (Request $request, Response $response) {
+    });
+
+    // 削除
+    $app->delete('/tickets/{id}', function (Request $request, Response $response) {
+    });
+
     $app->get('/[{name}]', function (Request $request, Response $response, array $args) use ($container) {
         // Sample log message
         $container->get('logger')->info("Slim-Skeleton '/' route");
