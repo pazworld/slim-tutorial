c759014c1e2ad53e345f8c904752db5beaffbc73
pazworld <paz@moudamepo.net>
2020-01-23 11:10:52 +0900

    Delete ticket

--- src/routes.php
+++ src/routes.php
@@ -82,7 +82,17 @@ return function (App $app) {
     });
 
     // 削除
-    $app->delete('/tickets/{id}', function (Request $request, Response $response) {
+    $app->delete('/tickets/{id}', function (Request $request, Response $response, array $args) {
+        $sql = 'SELECT * FROM tickets WHERE id = :id';
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute(['id' => $args['id']]);
+        $ticket = $stmt->fetch();
+        if (!$ticket) {
+            return $response->withStatus(404)->write('not found');
+        }
+        $stmt = $this->db->prepare('DELETE FROM tickets WHERE id = :id');
+        $stmt->execute(['id' => $ticket['id']]);
+        return $response->withRedirect('/tickets');
     });
 
     $app->get('/[{name}]', function (Request $request, Response $response, array $args) use ($container) {
